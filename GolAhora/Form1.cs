da019131b93323fe04ed3c2960ba49a6edaef259
fernando ariel franco <82460657+Atreesin@users.noreply.github.com>
2026-04-29 20:11:51 -0300

    Add files via upload

---
+++ GolAhora/Form1.cs
@@ -0,0 +1,10 @@
+namespace GolAhora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GolAhora/Utils/SessionManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GolAhora.Utils
+{
+    public static class SessionManager
+    {
+        public static string SessionId { get; set; }
+    }
+}
