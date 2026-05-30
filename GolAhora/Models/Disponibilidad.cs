427ba38183f1d5b05cf19e4578d9ab51b5e0d8b8
EnzoP101 <enzo.pedraza2195@gmail.com>
2026-05-29 23:40:45 -0300

    creacion de la clase disponibilidad

---
+++ GolAhora/Models/Disponibilidad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GolAhora.Models
+{
+    public class Disponibilidad
+    {
+        public int ID { get; set; }
+        public string Fecha { get; set; }
+        public string HoraInicio { get; set; }
+        public string HoraFin { get; set; }
+
+        public Disponibilidad() { }
+
+        public Disponibilidad(int id, string fecha, string horaInicio, string horaFin)
+        {
+            ID = id;
+            Fecha = fecha;
+            HoraInicio = horaInicio;
+            HoraFin = horaFin;
+        }
+    }
+}
